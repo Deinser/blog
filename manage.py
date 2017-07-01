c577ecabea8b33c82fa3d7c02dc75dc2411bd14e
Deinser <122744952@qq.com>
2017-07-01 14:45:29 +0800

    init

--- manage.py
+++ manage.py
@@ -4,6 +4,7 @@ from app import create_app,db
 from flask_script import Manager
 from app.models import User,Role,Post,Follow
 from flask_migrate import Migrate,MigrateCommand
+import os
 
 app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
