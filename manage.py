93cd756fea346919b1ef1b4a79b9c59987b8d4be
Deinser <122744952@qq.com>
2017-07-01 11:36:14 +0800

    init

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_script import Manager
 from app.models import User,Role,Post,Follow
 from flask_migrate import Migrate,MigrateCommand
 
-app=create_app('default')
+app=create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager=Manager(app)
 migrate=Migrate(app,db)
 
@@ -13,6 +13,12 @@ migrate=Migrate(app,db)
 def make_shell_context():
 	return dict(app=app,db=db,User=User,Role=Role,Post=Post,Follow=Follow)
 manager.add_command('db',MigrateCommand)
+
+
+@manager.command
+def datainit():
+	Role.insert_roles()
+	User.follow_me()
 
 @manager.command
 def deploy():
